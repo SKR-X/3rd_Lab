87b88b1b0c494c7ec9074c6dbeb4f3277dce7620
Avenir <kadomcev2004@gmail.com>
2022-10-19 01:19:08 +0300

    Algorithms_done

---
+++ Lab_files/Algorithms/array_shift/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Laboratory_A5
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int[] array_old = new int[]{1, 2, 3, 4, 5, 6};
+            int[] array_ans = new int[array_old.Length];
+            int m = 2;
+
+            for (int i = 0; i < array_old.Length; i++)
+            {
+                array_ans[(i+m)%(array_old.Length)] = array_old[i];
+            }
+
+            for (int i = 0; i < array_ans.Length; i++)
+            {
+                Console.Write($"{array_ans[i]} ");
+            }
+            return 0;
+            //yay, its done
+        }
+    }
+}
